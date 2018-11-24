81082d7967bda813b8fa216c29a4dc8c92315d40
Seyhun Saral <11707417+seyhunsaral@users.noreply.github.com>
2018-11-24 16:54:58 +0100

    comment about the built script

--- build_exe.cmd
+++ build_exe.cmd
@@ -1,3 +1,5 @@
+:: For proper executable generation, we install
+:: zBrac as a package and then generate exe file with cxfreeze 
 pip uninstall zbrac -y
 pip install ./src/zbrac --user
 python setup_cxfreeze.py build 
