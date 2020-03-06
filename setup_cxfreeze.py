f931274d22170f4bdd0f71481eb99fbdf2f260ea
annikahe <alhennes@web.de>
2020-03-06 11:59:40 +0100

    prepare for release 1.0.7

--- setup_cxfreeze.py
+++ setup_cxfreeze.py
@@ -13,7 +13,7 @@ exe = Executable(
 
 setup(
     name='zBrac',
-    version='1.0.5',
+    version='1.0.7',
     url='https://github.com/seyhunsaral/zbrac',
     license='GNU GPL-3',
     author='Ali Seyhun Saral, Anna Marie Schroeter',
