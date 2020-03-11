359f2f2633a72b3890093974e923c7691925ba35
Anna Schröter <amschroeter@online.de>
2020-03-11 13:01:57 +0100

    add add_escapes to unittest

--- src/zbrac/zbrac/functions-unittest.py
+++ src/zbrac/zbrac/functions-unittest.py
@@ -1,15 +1,21 @@
 import unittest
 from functions import remove_escapes
+from functions import add_escapes
 
 class TestStringMethods(unittest.TestCase):
 
     def test_remove_escapes(self):
         self.assertEqual(remove_escapes('hello no escapes here'),'hello no escapes here')
-        self.assertEqual(remove_escapes('normal escape \\!!'),'normal escape \!!')
-        self.assertEqual(remove_escapes('double escapes \\\\%'),'double escapes \\%')
-        self.assertEqual(remove_escapes('both \\"" \\\\5678'),'both \"" \\5678')
-        
-    
+        self.assertEqual(remove_escapes('escape \\"quotation\\"'),'escape \"quotation\"')
+        self.assertEqual(remove_escapes('escape escapes \\\\%'),'escape escapes \\%')
+        self.assertEqual(remove_escapes('both \\"quot\\" \\\\!'),'both \"quot\" \\!') 
+
+    def test_add_escapes(self):
+        self.assertEqual(add_escapes('hello no escapes here'),'hello no escapes here')
+        self.assertEqual(add_escapes('escape \"quotation\"'),'escape \\"quotation\\"')
+        self.assertEqual(add_escapes('escape escapes \\%'),'escape escapes \\\\%')
+        self.assertEqual(add_escapes('both \"quot\" \\!'),'both \\"quot\\" \\\\!') 
+
 
 if __name__ == '__main__':
     unittest.main()
