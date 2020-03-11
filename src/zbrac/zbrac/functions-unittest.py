4d1d117f1d860df4278849d5a558904463261418
Anna Schröter <amschroeter@online.de>
2020-03-11 14:13:50 +0100

    add create_own_list to unittest

--- src/zbrac/zbrac/functions-unittest.py
+++ src/zbrac/zbrac/functions-unittest.py
@@ -2,6 +2,7 @@ import unittest
 from functions import remove_escapes
 from functions import add_escapes
 from functions import get_matched_entries
+from functions import create_own_list
 
 class TestStringMethods(unittest.TestCase):
 
@@ -18,13 +19,19 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(add_escapes('both \"quot\" \\!'),'both \\"quot\\" \\\\!') 
 
     def test_get_matched_entries(self):
-        text = """This is [[a test file]]. 
+        text = """This is [[a \"test\" file]]. 
                Things there should be a bracket ([) in the output and there should be a [[replaced text]]."""
-        no_entries_text = """This is [[a test file. 
+        no_entries_text = """This is [[a \"test\" file. 
                              Things there should be a bracket ([) in the output and there should be a replaced text."""
-        entries = ['[[a test file]]','[[replaced text]]']
+        entries = ['[[a \"test\" file]]','[[replaced text]]']
         self.assertEqual(get_matched_entries(text),entries)
         self.assertFalse(get_matched_entries(no_entries_text))
+
+    def test_create_own_list(self):
+        list = ['[[a \"test\" file]]','[[replaced text]]']
+        empty_list = False
+        self.assertEqual(create_own_list(list),[['[[a "test" file]]','[[replaced text]]'],['a "test" file','replaced text']])
+        self.assertFalse(create_own_list(empty_list))
 
 if __name__ == '__main__':
     unittest.main()
